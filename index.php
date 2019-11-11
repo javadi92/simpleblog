59924bcfbb8ab03f77acf3c93fab4869ff10bf7e
Javadi <javadimehr01@gmail.com>
2019-11-11 19:17:10 +0330

    init commit

---
+++ index.php
@@ -0,0 +1,104 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand" href="#"><img src="https://faradars.org/wp-content/uploads/2013/12/FaraDars-Header-A.png" style="width: 110px"></img></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item active" style="direction: rtl;text-align: right">
+                        <a class="nav-link" href="#">خانه<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item" style="direction: rtl;text-align: right">
+                        <a class="nav-link" href="#">تماس با ما</a>
+                    </li>
+                </ul>
+                <form class="form-inline my-2 mr-auto">
+                    <input style="width: 200px" class="form-control mr-sm-2" type="search" placeholder="کلمه کلیدی" aria-label="Search">
+                    <button class="btn btn-outline-success my-2 my-sm-0 mr-1" type="submit">جستجو</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h4 class="my-5">وبلاگ آموزشی فرادرس</h4>
+                <div class="card" style="width: 100%;">
+                    <img src="img/forest.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">جنگل</h5>
+                        <p class="card-text">جنگل ها ریه های زمین هستند</p>
+                        <a href="#" class="btn btn-info">ادامه مطلب</a>
+                        <p></p>
+                        <div class="card-footer text-muted">
+                            انتشار در 27 اردیبهشت 98
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <br>
+                <div class="card" style="width: 100%;">
+                    <img src="img/mountain.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">کوهستان</h5>
+                        <p class="card-text">کوه ها نتیجه میلیون ها سال حرکت صفحات زمین هستند</p>
+                        <a href="#" class="btn btn-info">ادامه مطلب</a>
+                        <p></p>
+                        <div class="card-footer text-muted">
+                            انتشار در 27 اردیبهشت 98
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="card my-5" style="text-align: right">
+                    <div class="card-header">
+                        دسته بندی ها
+                    </div>
+                    <div class="card-body" style="direction: rtl">
+                        <div class="row">
+                            <div class="col-md-6 col-sm-6">
+                                <ul class="list-unstyled mb-0">
+                                    <li><a href="#">آموزش HTML</a></li>
+                                    <li><a href="#">آموزش CSS</a></li>
+                                    <li><a href="#">آموزش Javascript</a></li>
+                                </ul>
+                            </div>
+                            <div class="col-md-6 col-sm-6">
+                                <ul class="list-unstyled mb-0">
+                                    <li><a href="#">آموزش PHP</a></li>
+                                    <li><a href="#">آموزش ASP.NET</a></li>
+                                    <li><a href="#">آموزش JAVA</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <footer class="my-5 bg-light">
+        <div class="container">
+            <p class="m-0" style="text-align: center">ایجاد شده توسط فرهاد جوادی مهر</p>
+        </div>
+    </footer>
+</body>
+</html>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
